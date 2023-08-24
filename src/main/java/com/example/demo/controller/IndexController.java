6d9a800890d28264683c1715988a08282d7020a5
Haruki Uehara <74998274+harrki@users.noreply.github.com>
2023-08-24 12:38:26 +0900

    [ADD] GET /api/newsのエンドポイントを追加

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/")
+@RequestMapping("/api")
 @RestController
 public class IndexController {
     @GetMapping
     public String index() {
         return "hello springboot 3";
+    }
+
+    @GetMapping("/news")
+    public String news() {
+        return "hello springboot 3!";
     }
 }
