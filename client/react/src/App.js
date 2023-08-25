a76fb4b104dffcc18afc855bacd643a38b9cb4c3
soramikumo <sorao0324@gmail.com>
2023-08-25 14:20:06 +0900

    　[ADD] フロントエンド部分の追加

---
+++ client/react/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import NewsList from './components/NewsList';
+import VoiceVoxIcons from './components/VoiceVoxIcons';
+import ResPage from './components/ResPage';
+
+function App() {
+  const [selectedNews, setSelectedNews] = useState([]);
+
+  return (
+      <div className="App">
+        <Header />
+        <NewsList selectedNews={selectedNews} setSelectedNews={setSelectedNews} />
+        <VoiceVoxIcons selectedNews={selectedNews} />
+        <ResPage />
+      </div>
+  );
+}
+
+export default App;
