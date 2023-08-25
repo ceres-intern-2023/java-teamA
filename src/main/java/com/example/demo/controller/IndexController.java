2bc8d2563ac8254a6ab5ff36a4dd4d22493dfa12
Haruki Uehara <74998274+harrki@users.noreply.github.com>
2023-08-25 11:49:07 +0900

    [ADD] ニュース記事の取得機能の追加

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,20 +1,30 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.NewsDto;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RequestMapping("/api")
 @RestController
 public class IndexController {
     Map<Integer,String> newsMap = new HashMap<Integer, String>(){{
-        put(0,"");
-        put(1,"");
-        put(2,"");
-        put(3,"");
-        put(4,"");
+        put(0,"https://news.google.com/news/rss/headlines/section/topic/WORLD?hl=ja&gl=JP&ceid=JP:ja");
+        put(1,"https://news.google.com/news/rss/headlines/section/topic/NATION?hl=ja&gl=JP&ceid=JP:ja");
+        put(2,"https://news.google.com/news/rss/headlines/section/topic/BUSINESS?hl=ja&gl=JP&ceid=JP:ja");
+        put(3,"https://news.google.com/news/rss/headlines/section/topic/TECHNOLOGY?hl=ja&gl=JP&ceid=JP:ja");
+        put(4,"https://news.google.com/news/rss/headlines/section/topic/ENTERTAINMENT?hl=ja&gl=JP&ceid=JP:ja");
+        put(5,"https://news.google.com/news/rss/headlines/section/topic/SPORTS?hl=ja&gl=JP&ceid=JP:ja");
+        put(6,"https://news.google.com/news/rss/headlines/section/topic/SCIENCE?hl=ja&gl=JP&ceid=JP:ja");
+        put(7,"https://news.google.com/news/rss/headlines/section/topic/HEALTH?hl=ja&gl=JP&ceid=JP:ja");
     }};
 
     @GetMapping
@@ -23,12 +33,22 @@ public class IndexController {
     }
 
     @PostMapping("/news")
-    public String news(@RequestBody NewsDto param) {
+    public String news(@RequestBody NewsDto param) throws Exception {
         System.out.println(param.ids);
 
-        //TODO: 取得したIDを元にRSSからニュース記事を引っ張ってくる
+        List<String> newsText = new ArrayList<>();
 
-        String[] newsText = {"ニュース本文1","ニュース本文2","ニュース本文3"};
+        for(int id : param.ids){
+            String url = newsMap.get(id);
+            SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(url)));
+            for(int i=0;i<3;i++) {
+                String description = feed.getEntries().get(i).getDescription().getValue();
+                description = description.replaceAll("<.*?>", "");
+                description = description.replaceAll("&nbsp;", "");
+                description = description.replaceAll("Google ニュースですべての記事を見る", "");
+                newsText.add(description);
+            }
+        }
 
         //TODO: newsTextの中にある本文をChatGPTに送信し，要約したものを返す
 
